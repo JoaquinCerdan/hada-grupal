8700bc8b503bdc20246ae4b238d2202573621708
Pedro <pss68@alu.ua.es>
2021-04-16 12:34:59 +0200

    Clases CAD y EN de Anuncio y Lista deseos

--- iquea/CADAnuncio.cs
+++ iquea/CADAnuncio.cs
@@ -2,9 +2,30 @@
 
 public class CADAnuncio
 {
-	private String constring;
+	public String constring;
 	public CADAnuncio()
 	{
-
+		
 	}
+	
+
+	public bool createAnuncio (ENAnuncio anun)
+    {
+
+    }
+	public bool readAnuncio (ENAnuncio anun)
+    {
+
+    }
+	public bool updateAnuncio (ENAnuncio anun)
+    {
+
+    }
+
+	public bool deleteAnuncio (ENAnuncio anun)
+    {
+
+    }
+
+
 }
