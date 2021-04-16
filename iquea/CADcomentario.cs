1d1dcc859c0c954539a543e224faa8ff48cc757f
Daniel Bolaño Morillas <dbm39@gcloud.ua.es>
2021-04-16 12:20:40 +0200

    cabeceras de comentario y oferta creadas

--- iquea/CADcomentario.cs
+++ iquea/CADcomentario.cs
@@ -2,7 +2,29 @@
 
 public class CADcomentario
 {
+	private string constring;
+
 	public CADcomentario()
 	{
 	}
+
+    public bool createComentario(ENcomentario oferta)
+    {
+
+    }
+
+    public bool readComentario(ENcomentario oferta)
+    {
+
+    }
+
+    public bool deleteComentario(ENcomentario oferta)
+    {
+
+    }
+
+    public bool updateComentario(ENcomentario oferta)
+    {
+
+    }
 }
--- iquea/ENcomentario.cs
+++ iquea/ENcomentario.cs
@@ -2,7 +2,50 @@
 
 public class ENcomentario
 {
-	public ENcomentario()
-	{
-	}
+    private string comentario;
+    private int valoracion;
+
+    public string comentarioP
+    {
+        get { return this.comentario; }
+        set { this.comentario = value; }
+    }
+
+    public int valoracionP
+    {
+        get { return this.valoracion; }
+        set { this.valoracion = value; }
+    }
+
+    public ENcomentario()
+    {
+
+    }
+
+    public ENcomentario(string comentario,int valoracion)
+    {
+
+    }
+
+    public bool createComentario()
+    {
+
+    }
+
+    public bool readComentario()
+    {
+
+    }
+
+    public bool deleteComentario()
+    {
+
+    }
+
+    public bool updateComentario()
+    {
+
+    }
+
 }
+
