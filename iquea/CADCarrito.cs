0e60fc1155f7b61bc7afa9b6840714ae9d5e9281
JoaquinCerdan <jjcl4@gcloud.ua.es>
2021-04-20 12:27:44 +0200

    Arreglo definitivo

---
+++ iquea/CADCarrito.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class CADCarrito
+{
+	private string constring;
+
+    public bool createCarrito(ENCarrito carrito)
+    {
+
+    }
+    public bool readCarrito(ENCarrito carrito)
+    {
+
+    }
+    public bool updateCarrito(ENCarrito carrito)
+    {
+
+    }
+
+    public bool deleteCarrito(ENCarrito carrito)
+    {
+
+    }
+
+
+    public CADCarrito()
+	{
+	}
+}
