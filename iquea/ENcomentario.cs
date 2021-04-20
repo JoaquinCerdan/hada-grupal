3d490850f16237e03b894d2d1204dc995264862d
Daniel Bolaño Morillas <dbm39@gcloud.ua.es>
2021-04-20 11:30:35 +0200

    Encomentario implementado

--- iquea/ENcomentario.cs
+++ iquea/ENcomentario.cs
@@ -19,32 +19,62 @@ public class ENcomentario
 
     public ENcomentario()
     {
-
+        this.comentario = "";
+        this.valoracion = null;
     }
 
     public ENcomentario(string comentario,int valoracion)
     {
-
+        this.comentario = comentario;
+        //la valoración va desde 0 a 100.Si el valor dado no esta entre esos parametros no se aceptará
+        if(valoracion>=0 && valoracion <= 100)
+        {
+            this.valoracion = valoracion;
+        }
+        else
+        {
+            //crear error
+        }
     }
 
     public bool createComentario()
     {
+        bool respuesta;
 
+        CADcomentario comentario=new CADcomentario();
+        respuesta = comentario.createComentario(this);
+
+        return respuesta;
     }
 
     public bool readComentario()
     {
+        bool respuesta;
 
+        CADcomentario comentario = new CADcomentario();
+        respuesta = comentario.readComentario(this);
+
+        return respuesta;
     }
 
     public bool deleteComentario()
     {
+        bool respuesta;
 
+        CADcomentario comentario = new CADcomentario();
+        respuesta = comentario.deleteComentario(this);
+
+        return respuesta;
     }
 
     public bool updateComentario()
     {
+        bool respuesta;
 
+        CADcomentario comentario = new CADcomentario();
+        respuesta = comentario.updateComentario(this);
+
+        return respuesta;
     }
 
 }
--- iquea/ENoferta.cs
+++ iquea/ENoferta.cs
@@ -19,12 +19,18 @@ public class ENoferta
 
     public ENoferta()
     {
-
+        this.descuento = null;
+        this.fechaValidez = null;
     }
 
     public ENoferta(double descuento, DateTime fechaValidez)
     {
-
+        //solo se aceptará el descuento si es mayor que 0 y menor que 100(el descuento funciona en forma de proporcion sobre el precio)
+        if(descuento>0 && descuento < 100)
+        {
+            this.descuento = descuento;
+        }
+        
     }
 
     public bool createOferta()
