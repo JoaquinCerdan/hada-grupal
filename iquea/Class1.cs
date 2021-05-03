94cea0ca86957fdd9c589e863b6a0cb8ce6fc8c7
Daniel Bolaño Morillas <dbm39@gcloud.ua.es>
2021-05-03 17:08:24 +0200

    solucion a errores

--- iquea/Class1.cs
+++ iquea/Class1.cs
@@ -11,17 +11,4 @@ namespace iquea
     {
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace iquea
-{
-    public class Class1
-    {
-    }
-}
->>>>>>> 9be9fbdacb0a588ced5c8eacd5cf15ca7492f529
