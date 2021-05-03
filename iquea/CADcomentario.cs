ba27799346d9327bd23c25d4ea571dc3e1719a3e
JoaquinCerdan <jjcl4@gcloud.ua.es>
2021-05-03 16:54:59 +0200

    Arreglo

---
+++ iquea/CADcomentario.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+public class CADcomentario
+{
+	private string constring;
+
+	public CADcomentario()
+	{
+        constring = "";
+	}
+
+    public bool createComentario(ENcomentario comentario)
+    {
+        bool respuesta = false;
+        SqlConnection a = new SqlConnection(constring);
+        try
+        {
+            a.Open();
+            String orden = "Insert Into iquea.comentario (id, comentario, valoracion) VALUES ('" + comentario.idP + "' , '" + comentario.comentarioP + "' , '" + comentario.valoracionP + ")";
+            SqlCommand comando = new SqlCommand(orden, a);
+            comando.ExecuteNonQuery();
+
+            respuesta = true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("User operation has failed. Error: {0}", e.Message);
+            return false;
+        }
+
+        finally
+        {
+            a.Close();
+        }
+
+        return respuesta;
+    }
+
+    public bool readComentario(ENcomentario comentario)
+    {
+        bool respuesta = false;
+        SqlConnection a = new SqlConnection(constring);
+
+        try
+        {
+            a.Open();
+            SqlCommand comando = new SqlCommand("Select * from iquea.comentario where id = '" + comentario.idP + "'", a);
+            SqlDataReader dr = comando.ExecuteReader();
+
+            dr.Read();
+            comentario.comentarioP = dr["comentario"].ToString();
+            comentario.valoracionP = Convert.ToInt32(dr["valoracion"]);
+
+            dr.Close();
+            a.Close();
+            respuesta = true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("User operation has failed. Error: {0}", e.Message);
+            return false;
+        }
+
+        finally
+        {
+            a.Close();
+        }
+
+        return respuesta;
+    }
+
+    public bool deleteComentario(ENcomentario comentario)
+    {
+        bool respuesta = false;
+        SqlConnection a = new SqlConnection(constring);
+        try
+        {
+            a.Open();
+            SqlCommand comando = new SqlCommand("DELETE FROM iquea.comentario WHERE id = '" + comentario.idP + "'", a);
+            comando.ExecuteNonQuery();
+
+            respuesta = true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("User operation has failed. Error: {0}", e.Message);
+            return false;
+        }
+
+        finally
+        {
+            a.Close();
+        }
+
+        return respuesta;
+    }
+
+    public bool updateComentario(ENcomentario comentario)
+    {
+        bool respuesta = false;
+        SqlConnection a = new SqlConnection(constring);
+        try
+        {
+            a.Open();
+            SqlCommand comando = new SqlCommand("UPDATE iquea.comentario SET comentario = '" + comentario.comentarioP + "' , valoracion = '" + comentario.valoracionP + "' WHERE id = '" + comentario.idP + "'", a);
+            comando.ExecuteNonQuery();
+
+            respuesta = true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("User operation has failed. Error: {0}", e.Message);
+            return false;
+        }
+
+        finally
+        {
+            a.Close();
+        }
+
+        return respuesta;
+    }
+}
