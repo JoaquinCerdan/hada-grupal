0e642f74fd2801073024c16d8d5702a932a7c0f9
AdamBrahimGarcia <bluejayway817@gmail.com>
2021-05-11 13:11:43 +0200

    Modificaciones en CAD y EN

--- iquea/CADAdministrador.cs
+++ iquea/CADAdministrador.cs
@@ -12,26 +12,110 @@ namespace iquea
 
         public CADAdministrador()
         {
+            constring = "";
         }
 
-        public bool createAdministrador(ENAdministrador ad)
+        public bool createAdministrador(ENAdministrador administrador)
         {
-            return false;
+            SqlConnection conexion = null;
+            bool creado = false;
+            try
+            {
+                conexion = new SqlConnection(constring);
+                conexion.Open();
+                String comando = "Insert INTO iquea.Administrador (id) VALUES (" + administrador.intId + ")";
+                SqlCommand ejecucion = new SqlCommand(comando, conexion);
+                ejecucion.ExecuteNonQuery();
+                creado = true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("User operation has failed.Error: {0}", e.Message);
+                creado = false;
+            }
+            finally
+            {
+                conexion.Close();
+            }
+            return creado;
         }
 
-        public bool readAdministrador(ENAdministrador ad)
+        public bool readAdministrador(ENAdministrador administrador)
         {
-            return false;
+            SqlConnection conexion = null;
+            bool creado = false;
+            try
+            {
+                conexion = new SqlConnection(constring);
+                conexion.Open();
+                String comando = "Select * FROM iquea.Administrador WHERE id = '" + administrador.intId + "'";
+                SqlCommand ejecucion = new SqlCommand(comando, conexion);
+                SqlDataReader leer = ejecucion.ExecuteReader();
+                leer.Read();
+                leer.Close();
+                conexion.Close();
+                creado = true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("User operation has failed.Error: {0}", e.Message);
+                creado = false;
+            }
+            finally
+            {
+                conexion.Close();
+            }
+            return creado;
         }
 
-        public bool deleteAdministrador(ENAdministrador ad)
+        public bool deleteAdministrador(ENAdministrador administrador)
         {
-            return false;
+            SqlConnection conexion = null;
+            bool creado = false;
+            try
+            {
+                conexion = new SqlConnection(constring);
+                conexion.Open();
+                String comando = "DELETE FROM iquea.Administrador WHERE id = '" + administrador.getId() + "'";
+                SqlCommand ejecucion = new SqlCommand(comando, conexion);
+                ejecucion.ExecuteNonQuery();
+                creado = true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("User operation has failed.Error: {0}", e.Message);
+                creado = false;
+            }
+            finally
+            {
+                conexion.Close();
+            }
+            return creado;
         }
 
-        public bool updateAdministrador(ENAdministrador ad)
+        public bool updateAdministrador(ENAdministrador administrador)
         {
-            return false;
+            SqlConnection conexion = null;
+            bool creado = false;
+            try
+            {
+                conexion = new SqlConnection(constring);
+                conexion.Open();
+                SqlCommand comando = new SqlCommand("UPDATE iquea.Usuario SET id = '" + administrador.intID + "' WHERE id = '" + administrador.intID + "'", conexion);
+                SqlCommand ejecucion = new SqlCommand(comando, conexion);
+                ejecucion.ExecuteNonQuery();
+                creado = true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("User operation has failed.Error: {0}", e.Message);
+                creado = false;
+            }
+            finally
+            {
+                conexion.Close();
+            }
+            return creado;
         }
     }
 }
