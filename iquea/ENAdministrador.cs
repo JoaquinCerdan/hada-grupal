4a385a45728dbc08e5ded63a67e66bea45fa5b5a
Daniel Bolaño Morillas <dbm39@gcloud.ua.es>
2021-05-20 19:24:17 +0200

    clases pasadas del develop

---
+++ iquea/ENAdministrador.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iquea
+{
+    class ENAdministrador
+    {
+        private int id;
+
+        public int intId
+        {
+            get { return this.id; }
+            set { this.id = value; }
+        }
+
+        public ENAdministrador()
+        {
+
+        }
+
+        public bool createAdministrador()
+        {
+            bool isTrue;
+            CADAdministrador administrador = new CADAdministrador();
+            isTrue = administrador.createAdministrador(this);
+            return isTrue;
+        }
+
+        public bool readAdministrador()
+        {
+            bool isTrue;
+            CADAdministrador administrador = new CADAdministrador();
+            isTrue = administrador.readAdministrador(this);
+            return isTrue;
+        }
+
+        public bool updateAdministrador()
+        {
+            bool isTrue;
+            CADAdministrador administrador = new CADAdministrador();
+            isTrue = administrador.updateAdministrador(this);
+            return isTrue;
+        }
+
+        public bool deleteAdministrador()
+        {
+            bool isTrue;
+            CADAdministrador administrador = new CADAdministrador();
+            isTrue = administrador.deleteAdministrador(this);
+            return isTrue;
+        }
+
+    }
+}
