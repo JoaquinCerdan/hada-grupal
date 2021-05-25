27030080baf2fc14450f0e3b62e996c45cba01ce
Álvaro Lario Sánchez <als106@alu.ua.es>
2021-05-25 12:34:16 +0200

    .

--- iqueaWeb/CrearArticulo.aspx.cs
+++ iqueaWeb/CrearArticulo.aspx.cs
@@ -11,8 +11,6 @@ namespace iqueaWeb
 
         protected void Button2_Click(object sender, EventArgs e)
         {
-            ENArticulo en = new ENArticulo(TextBox5.Text, Convert.ToDouble(TextBox6.Text), TextBox7.Text, Convert.ToInt32(TextBox8.Text));
-            en.createArticulo();
         }
     }
 }
