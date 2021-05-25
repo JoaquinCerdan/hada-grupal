f313c77e4c3dca7f07a981b59ddc7d570f45501d
Daniel Bolaño Morillas <dbm39@gcloud.ua.es>
2021-05-25 12:30:41 +0200

    Registrarse implementado

--- iquea/CADUsuario.cs
+++ iquea/CADUsuario.cs
@@ -25,7 +25,7 @@ public class CADUsuario
         {
             conexion = new SqlConnection(constring);
             conexion.Open();
-            String comando = "Insert INTO iquea.Usuario (email, contra, datosBancarios, nombre, direccion, numTelefono) VALUES (" + usuario.stringEmail + " , " + usuario.stringContra + " , " + usuario.stringDatosBancarios + " , " + usuario.stringNombre + " , " + usuario.stringDireccion + " , " + usuario.intNumTelefono + ")";
+            String comando = "Insert INTO dbo.Usuarios (email, contra, datosBancarios, nombre, direccion, numTelefono) VALUES ('" + usuario.stringEmail + "' , '" + usuario.stringContra + "' , '" + usuario.stringDatosBancarios + "' , '" + usuario.stringNombre + "' , '" + usuario.stringDireccion + "' , " + usuario.intNumTelefono + ")";
             SqlCommand ejecucion = new SqlCommand(comando, conexion);
             ejecucion.ExecuteNonQuery();
             creado = true;
@@ -54,7 +54,7 @@ public class CADUsuario
         {
             conexion = new SqlConnection(constring);
             conexion.Open();
-            String comando = "Select * FROM iquea.Usuario WHERE email = '" + usuario.stringEmail + "'";
+            String comando = "Select * FROM dbo.Usuarios WHERE email = '" + usuario.stringEmail + "'";
             SqlCommand ejecucion = new SqlCommand(comando, conexion);
             SqlDataReader leer = ejecucion.ExecuteReader();
             leer.Read();
@@ -87,7 +87,7 @@ public class CADUsuario
         {
             conexion = new SqlConnection(constring);
             conexion.Open();
-            String comando = "DELETE FROM iquea.Usuario WHERE email = '" + usuario.stringEmail + "'";
+            String comando = "DELETE FROM dbo.Usuarios WHERE email = '" + usuario.stringEmail + "'";
             SqlCommand ejecucion = new SqlCommand(comando, conexion);
             ejecucion.ExecuteNonQuery();
             creado = true;
@@ -117,7 +117,7 @@ public class CADUsuario
         {
             conexion = new SqlConnection(constring);
             conexion.Open();
-            SqlCommand comando = new SqlCommand("UPDATE iquea.Usuario SET email = '" + usuario.stringEmail + "' , contra = '" + usuario.stringContra + "' , datosBancarios = '" + usuario.stringDatosBancarios + "' , nombre = '" + usuario.stringNombre + "' , direccion = '" + usuario.stringDireccion + "' , numTelefono = '" + usuario.intNumTelefono + "' WHERE id = '" + usuario.stringEmail + "'", conexion);
+            SqlCommand comando = new SqlCommand("UPDATE dbo.Usuarios SET email = '" + usuario.stringEmail + "' , contra = '" + usuario.stringContra + "' , datosBancarios = '" + usuario.stringDatosBancarios + "' , nombre = '" + usuario.stringNombre + "' , direccion = '" + usuario.stringDireccion + "' , numTelefono = '" + usuario.intNumTelefono + "' WHERE id = '" + usuario.stringEmail + "'", conexion);
             //SqlCommand ejecucion = new SqlCommand(comando, conexion);
             comando.ExecuteNonQuery();
             creado = true;
--- iqueaWeb/Registrarse.aspx
+++ iqueaWeb/Registrarse.aspx
@@ -8,17 +8,16 @@
         <p>
             Correo:&nbsp;<asp:TextBox ID="Correo" runat="server"></asp:TextBox>
             <asp:RequiredFieldValidator ID="CorreoRequired" runat="server" ErrorMessage="Tienes que introducir un correo." ControlToValidate="Correo" ForeColor="#CC0000"></asp:RequiredFieldValidator>
-            <!--&nbsp;&nbsp;<asp:RegularExpressionValidator ID="RegularExpressionCorreo" runat="server" ErrorMessage="Los datos dados no son un email." ControlToValidate="Correo" ForeColor="#CC0000" ValidationExpression="/^(([^<>()\[\]\.,;:\s@\”]+(\.[^<>()\[\]\.,;:\s@\”]+)*)|(\”.+\”))@(([^<>()[\]\.,;:\s@\”]+\.)+[^<>()[\]\.,;:\s@\”]{2,})$/"></asp:RegularExpressionValidator>-->
         </p>
         <p>
             Contraseña:&nbsp;<asp:TextBox ID="Contraseña1" runat="server"></asp:TextBox>
             <asp:RequiredFieldValidator ID="PasswordRequired1" runat="server" ErrorMessage="Tienes que introducir una contraseña." ControlToValidate="Contraseña1" ForeColor="#CC0000"></asp:RequiredFieldValidator>
-            <!--&nbsp;&nbsp;<asp:RangeValidator ID="RangePassword1" runat="server" ErrorMessage="La contraseña tiene que tener entre 8 y 20 dígitos." ForeColor="#CC0000" ControlToValidate="Contraseña1" MinimumValue="20" MaximumValue="8"></asp:RangeValidator>-->
+        
         </p>
         <p>
             Repetir Contraseña:&nbsp;<asp:TextBox ID="Contraseña2" runat="server"></asp:TextBox>
             <asp:RequiredFieldValidator ID="PasswordRequired2" runat="server" ErrorMessage="Tienes que introducir una contraseña." ControlToValidate="Contraseña2" ForeColor="#CC0000"></asp:RequiredFieldValidator>
-            <!--&nbsp;&nbsp;<asp:RangeValidator ID="RangePassword2" runat="server" ErrorMessage="La contraseña tiene que tener entre 8 y 20 dígitos." ForeColor="#CC0000" ControlToValidate="Contraseña2" MaximumValue="8" MinimumValue="20"></asp:RangeValidator>-->
+            
         </p>
         <p>Nombre:&nbsp;<asp:TextBox ID="Nombre" runat="server"></asp:TextBox>
         <asp:RequiredFieldValidator ID="NombreRequired" runat="server" ErrorMessage="Tienes que introducir tu nombre." ControlToValidate="Nombre" ForeColor="#CC0000"></asp:RequiredFieldValidator>
@@ -34,6 +33,7 @@
         </p>
 
         &nbsp;&nbsp;&nbsp;<asp:Button ID="CrearUsuario" runat="server" Text="Registrar" OnClick="CrearUsuario_Click" />
+        <asp:Label ID="etiqueta" runat="server" Text="Aviso"></asp:Label>
 
     </div>
 
