53c1ec0abd71be3db04fc5a4bc0bb98f92c5f175
JoaquinCerdan <jjcl4@gcloud.ua.es>
2021-05-25 12:26:40 +0200

    Subido EN/CAD Articulo y Categoria

--- iquea/ENCategoria.cs
+++ iquea/ENCategoria.cs
@@ -1,41 +1,92 @@
-﻿/// <summary>
+﻿using System;
+
+/// <summary>
 /// Summary description for ENCategoria
 /// </summary>
 public class ENCategoria
 {
+	private int id;
 
-    private string nombre;
+	public int intId
+	{
+		get { return id; }
+		set { id = value; }
+	}
 
-    private string descripcion;
+	private string nombre;
 
-    public ENCategoria()
-    {
+	public string stringNombre
+	{
+		get { return nombre; }
+		set { nombre = value; }
+	}
 
-    }
+	private string descripcion;
 
-    public ENCategoria(string nombre, string descripcion)
-    {
+	public string stringDescripcion
+	{
+		get { return descripcion; }
+		set { descripcion = value; }
+	}
 
-    }
+	public ENCategoria()
+	{
+		intId = 0;
+		stringNombre = "";
+		stringDescripcion = "";
+	}
 
-    public bool createCategoria()
-    {
-        return false;
-    }
+	public ENCategoria(int id, string nombre, string descripcion)
+	{
+		intId = id;
+		stringNombre = nombre;
+		stringDescripcion = descripcion;
+	}
 
-    public bool updateCategoria()
-    {
-        return false;
-    }
+	public bool createCategoria()
+	{
+		CADCategoria categoria = new CADCategoria();
+		bool created = false;
+		if (!categoria.readCategoria(this))
+			created = categoria.createCategoria(this);
+		return created;
+	}
 
-    public bool readCategoria()
-    {
-        return false;
-    }
+	public bool updateCategoria()
+	{
+		ENCategoria aux = new ENCategoria();
+		CADCategoria categoria = new CADCategoria();
 
-    public bool deleteCategoria()
-    {
-        return false;
-    }
+		bool updated = false;
+		aux.id = this.id;
+		aux.nombre = this.nombre;
+		aux.descripcion = this.descripcion;
+
+		if (categoria.readCategoria(this))
+		{
+			this.id = aux.id;
+			this.nombre = aux.nombre;
+			this.descripcion = aux.descripcion;
+			updated = categoria.updateCategoria(this);
+		}
+
+		return updated;
+	}
+
+	public bool readCategoria()
+	{
+		CADCategoria categoria = new CADCategoria();
+		bool read = categoria.readCategoria(this);
+		return read;
+	}
+
+	public bool deleteCategoria()
+	{
+		CADCategoria categoria = new CADCategoria();
+		bool read = false;
+		if (categoria.readCategoria(this))
+			read = categoria.deleteCategoria(this);
+		return read;
+	}
 
 }
