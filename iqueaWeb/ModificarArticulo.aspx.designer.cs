f5a3802bd1c1c2c415307f7a4512eb7b3b87288b
Álvaro Lario Sánchez <als106@alu.ua.es>
2021-06-02 20:18:29 +0200

    Admin

--- iqueaWeb/ModificarArticulo.aspx.designer.cs
+++ iqueaWeb/ModificarArticulo.aspx.designer.cs
@@ -24,6 +24,24 @@ namespace iqueaWeb
         protected global::System.Web.UI.WebControls.TextBox TextBox4;
 
         /// <summary>
+        /// Control RequiredFieldValidator1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
+
+        /// <summary>
+        /// Control RegularExpressionValidator2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator RegularExpressionValidator2;
+
+        /// <summary>
         /// Control TextBox5.
         /// </summary>
         /// <remarks>
@@ -31,6 +49,15 @@ namespace iqueaWeb
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TextBox5;
+
+        /// <summary>
+        /// Control RequiredFieldValidator2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator2;
 
         /// <summary>
         /// Control TextBox6.
@@ -42,6 +69,24 @@ namespace iqueaWeb
         protected global::System.Web.UI.WebControls.TextBox TextBox6;
 
         /// <summary>
+        /// Control RequiredFieldValidator5.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator5;
+
+        /// <summary>
+        /// Control RegularExpressionValidator1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator RegularExpressionValidator1;
+
+        /// <summary>
         /// Control TextBox7.
         /// </summary>
         /// <remarks>
@@ -51,6 +96,15 @@ namespace iqueaWeb
         protected global::System.Web.UI.WebControls.TextBox TextBox7;
 
         /// <summary>
+        /// Control RequiredFieldValidator3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator3;
+
+        /// <summary>
         /// Control TextBox8.
         /// </summary>
         /// <remarks>
@@ -58,6 +112,15 @@ namespace iqueaWeb
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TextBox8;
+
+        /// <summary>
+        /// Control RequiredFieldValidator4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator4;
 
         /// <summary>
         /// Control Button2.
--- iqueaWeb/ModificarOferta.aspx
+++ iqueaWeb/ModificarOferta.aspx
@@ -3,10 +3,18 @@
     <p>
         &nbsp;Código de la Oferta:
         <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator1" 
+            runat="server" ErrorMessage=" *Campo Obligatorio" 
+            forecolor="red" ControlToValidate="TextBox2">
+        </asp:RequiredFieldValidator>
         &nbsp;</p>
     <p>
         &nbsp;Descuento:
         <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator2" 
+            runat="server" ErrorMessage=" *Campo Obligatorio" 
+            forecolor="red" ControlToValidate="TextBox3">
+        </asp:RequiredFieldValidator>
     </p>
     <p>
         &nbsp;Fecha de Validez: &nbsp;<input id="date" type="date" value="0000-00-00"></p>
