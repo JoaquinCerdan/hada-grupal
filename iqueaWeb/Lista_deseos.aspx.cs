5958e41516fe2c6debf535db65a8001c7b5db809
Pedro <pss68@alu.ua.es>
2021-06-04 14:34:42 +0200

    empezado lo del gridview listadeseos

--- iqueaWeb/Lista_deseos.aspx.cs
+++ iqueaWeb/Lista_deseos.aspx.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
 
 namespace iqueaWeb
 {
     public partial class Lista_deseos : System.Web.UI.Page
     {
+        SqlConnection c = new SqlConnection();
         protected void Page_Load(object sender, EventArgs e)
         {
             if (Session["email"] == null)
@@ -16,6 +21,28 @@ namespace iqueaWeb
 
             }
         }
+
+        void Load_GridData()
+        {
+            try
+            {
+                c.Open();
+                SqlDataAdapter sqa = new SqlDataAdapter("Select * From [dbo].[Articulo] where Articulo.Temporada='Moderno'", c);
+                DataSet ds = new DataSet();
+                sqa.Fill(ds);
+                GridView.DataSource = ds;
+                GridView.DataBind();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The operation has failed.Error: {0}", e.Message);
+            }
+            finally
+            {
+                c.Close();
+            }
+        }
+
 
         protected void Button1_Click(object sender, EventArgs e)
         {
